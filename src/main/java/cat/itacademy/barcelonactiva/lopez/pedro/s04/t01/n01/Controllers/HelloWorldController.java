8979638ca84884a0c76e8b99844b1aa0d11f2970
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-24 12:31:12 +0100

    transforms the program in a API REST by using diverse annotation like
    Controller, GetMapping, Request Mapping, ResponseBody, RequestParam,
    PathVariable
    
    tested by postman collection; test passed

--- src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s04/t01/n01/Controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s04/t01/n01/Controllers/HelloWorldController.java
@@ -2,54 +2,29 @@ package cat.itacademy.barcelonactiva.lopez.pedro.s04.t01.n01.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("http://localhost:9000/")
 public class HelloWorldController {
 	
-	@GetMapping("http://localhost:9000/HelloWorld")
-	public String hi(@RequestParam("UNKNOWN") String name) {
+	@GetMapping("/HelloWorld")
+	@ResponseBody
+	public String hi(@RequestParam(defaultValue = "UNKNOWN") String name) {
 		return "Hi " + name + ", you are in";
 	}
 	
-	public String hi2(String name) {
+	@RequestMapping(value = {"/HelloWorld/{n}"})
+	@ResponseBody
+	public String hi2(@PathVariable(name = "n") String name) {
 		return "Hi " + name + ", you are in";
 	}
 
-	
-	
 	/*
-
-En el archivo application.properties, configura la variable server.port con el valor 9000.
-Convertiremos esta aplicación en una API REST:
- 	Dependiendo del package principal, crea otro subpackage llamado Controllers, y dentro de él, 
-	añade la clase HelloWorldController.
-	Deberá tener dos métodos:
- 		String saluda
- 		String saluda2
-
-• Estos dos métodos recibirán un parámetro String llamado nombre, y devolverán la frase:
-• "Hola, "+ nombre +". Estás ejecutando un proyecto Maven”.
-
-El primer método responderá a una petición GET, y deberá ser configurado para recibir el parámetro 
-como un RequestParam. El parámetro "nombre" tendrá el valor predeterminado “UNKNOWN”.
-Deberá responder a:
-• http://localhost:9000/HelloWorld
-• http://localhost:9000/HelloWorld?nombre=Mi nombre
-
-El segundo método responderá a una petición GET, y deberá ser configurado para recibir el parámetro 
-como una PathVariable. El parámetro "nombre" será opcional.
-Deberá responder a:
-• http://localhost:9000/HelloWorld2
-• http://localhost:9000/HelloWorld2/mi nombre
-
-Muy Importante
-Además del enlace a Git de la tarea resuelta, tendrás que incluir al menos dos enlaces diferentes de los recursos que te hemos proporcionado en el campus, que te hayan servido o lo hubieran podido hacer, para resolver la totalidad de la tarea o algunas partes.
-	
+	 * https://www.baeldung.com/spring-optional-path-variables
+	 * https://www.baeldung.com/spring-request-param
 	 */
-
-	
 	
 }
